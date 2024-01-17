2b30f572d65f19b2ffcfa86b9b9faa54a81d294f
Alfonso Patron <c9alfonsopatron@hotmail.com>
2024-01-16 22:34:22 -0700

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Alfonso Patron on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
